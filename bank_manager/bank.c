97935c961a36c699e19100ea55513f19c9fc4b8a
Sam Kaveh <seriousemail178@gmail.com>
2025-02-04 13:07:31 +1100

    Added createAccount Function

--- bank_manager/bank.c
+++ bank_manager/bank.c
@@ -6,10 +6,24 @@ struct Account {
 	float balance;
 };
 
+struct Account createAccount(char* number, float balance);
+
 int main() {
-	struct Account test_acc = {"1234567890", 1500};
+	char* test_number = "1234567890";
+	float test_balance = 1500;
+
+	struct Account test_acc = createAccount(test_number, test_balance);
 	
 	printf("Test Account:\nNumber: %s\nBalance: %.2f\n", test_acc.number, test_acc.balance);
 	
 	return 0;
 }	
+
+struct Account createAccount(char* number, float balance) {
+	struct Account new_account;
+
+	new_account.number = number;
+	new_account.balance = balance;
+	
+	return new_account;
+}
