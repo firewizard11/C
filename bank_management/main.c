c2e9ee26bb748f5942e5fce2e406eba63dac20c8
bobby <blahblah@gmail>
2025-02-07 21:29:52 +1100

    Defined openAccount

--- bank_management/main.c
+++ bank_management/main.c
@@ -8,15 +8,18 @@ typedef struct {
 } Bank;
 
 Bank createBank();
-void openAccount(char number[8]);
-static int searchBank(char number[8]);
-void closeAccount(char number[8]);
-void checkAccount(char number[8]);
-void depositAccount(char number[8], float amount);
-float withdrawAccount(char number[8], float amount);
+void openAccount(Bank* bank, char number[8]);
+static int searchBank(Bank* bank, char number[8]);
+void closeAccount(Bank* bank, char number[8]);
+void checkAccount(Bank* bank, char number[8]);
+void depositAccount(Bank* bank, char number[8], float amount);
+float withdrawAccount(Bank* bank, char number[8], float amount);
 
 int main() {
     Bank test_bank = createBank();
+
+    openAccount(&test_bank, "12345678");
+    printAccount(test_bank.open_acc[0]);
 
     return 0;
 }
@@ -26,4 +29,9 @@ Bank createBank() {
     new_bank.num_acc = 0;
 
     return new_bank;
+}
+
+void openAccount(Bank* bank, char number[8]) {
+    bank->open_acc[bank->num_acc] = createAccount(number, 0);
+    bank->num_acc += 1;
 }
