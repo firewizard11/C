f727bc819476a2cffaf6b04d1330af514d465e2e
bobby <blahblah@gmail>
2025-02-07 13:31:37 +1100

    - Made Account into a type so I don't have to write struct Account all   the time - Implemented depositAccount

--- bank_manager/bank.c
+++ bank_manager/bank.c
@@ -1,29 +1,41 @@
 #include <stdio.h>
 #include <string.h>
 
-struct Account {
+typedef struct {
 	char* number;
 	float balance;
-};
+} Account;
 
-struct Account createAccount(char* number, float balance);
+Account createAccount(char* number, float balance);
+void depositAccount(Account* acc, float amount);
+float withdrawAccount(Account* acc, float amount);
 
 int main() {
 	char* test_number = "1234567890";
 	float test_balance = 1500;
 
-	struct Account test_acc = createAccount(test_number, test_balance);
+	Account test_acc = createAccount(test_number, test_balance);
 	
 	printf("Test Account:\nNumber: %s\nBalance: %.2f\n", test_acc.number, test_acc.balance);
+
+	printf("\nTest Deposit\n");
+
+	depositAccount(&test_acc, 1500);
+
+	printf("Balance: %.2f\n", test_acc.balance);
 	
 	return 0;
 }	
 
-struct Account createAccount(char* number, float balance) {
-	struct Account new_account;
+Account createAccount(char* number, float balance) {
+	Account new_account;
 
 	new_account.number = number;
 	new_account.balance = balance;
 	
 	return new_account;
+}
+
+void depositAccount(Account* acc, float amount) {
+	acc->balance += amount;
 }
