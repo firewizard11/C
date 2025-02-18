ff15b94a08be1d2e1499382f9900c6bc67bf00db
bobby <blahblah@gmail>
2025-02-18 16:15:31 +1100

    Fixed createAccount and testing error and made depositAccount

--- bank_management_system/src/main.c
+++ bank_management_system/src/main.c
@@ -31,15 +31,35 @@ int main() {
 
 	acc = createAccount(test_num, test_balance);
 
+	if (acc == NULL) {
+		printf("Error: Account creation failed\n");
+		return 1;
+	}
+	
+	depositAccount(acc, 1500);
+
+	if (acc->balance != 3000) {
+		printf("Error: Deposit Failed %.2f", acc->balance);
+		return 1;
+	}
+
 	printf("=== Test Account ===\n");
-	printf("Account Number: %s\n", test_num);
-	printf("Account Balance: %.2f\n", test_balance);
+	printf("Account Number: %s\n", acc->num);
+	printf("Account Balance: %.2f\n", acc->balance);
 
 	return 0;
 }
 
 
 Account* createAccount(char num[10], float balance) {
-	Account new_acc = {*num, balance};
+	static Account new_acc;
+	strcpy(new_acc.num, num); 
+	new_acc.balance = balance;
 	return &new_acc;
 }
+
+void depositAccount(Account* acc, float amount) {
+	acc->balance += amount;
+}
+
+float withdrawAccount(Account* acc, float amount);
