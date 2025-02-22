88151ca3960ca0e922c56044eecc148568e4685d
Sam Kaveh <seriousemail178@gmail.com>
2025-02-22 12:52:07 +1100

    Moved validation functions from main.c to account.c\.h

--- bank_management_system/include/account.h
+++ bank_management_system/include/account.h
@@ -7,3 +7,5 @@ typedef struct {
 Account* createAccount(char num[10], char pin[5], float balance);
 void depositAccount(Account* acc, float amount);
 float withdrawAccount(Account* acc, float amount);
+int validate_acc_num(char acc_num[10]);
+int validate_pin(char pin[5]);
--- bank_management_system/src/account.c
+++ bank_management_system/src/account.c
@@ -1,4 +1,5 @@
 #include <string.h>
+#include <ctype.h>
 #include "../include/account.h"
 
 Account* createAccount(char num[10], char pin[5], float balance) {
@@ -24,4 +25,36 @@ float withdrawAccount(Account* acc, float amount) {
 
 	acc->balance -= amount;
 	return w_amount;
+}
+
+int validate_acc_num(char acc_num[10]) {
+	int length = strlen(acc_num);
+
+	if (length != 9) {
+		return 1;
+	}
+
+	for (int i = 0; i < length; i++) {
+		if (isdigit(acc_num[i]) == 0) {
+			return 1;
+		}
+	}
+
+	return 0;
+}
+
+int validate_pin(char pin[5]) {
+	int length = strlen(pin);
+
+	if (length != 4) {
+		return 1;
+	}
+
+	for (int i = 0; i < length; i++) {
+		if (isdigit(pin[i]) == 0) {
+			return 1;
+		}
+	}
+
+	return 0;
 }
