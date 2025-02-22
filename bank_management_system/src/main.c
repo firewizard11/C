55ed9f463719b9da5dce20689d29a2cfccbc94dc
Sam Kaveh <seriousemail178@gmail.com>
2025-02-22 12:08:29 +1100

    Finished account number validation validate_acc_num

--- bank_management_system/src/main.c
+++ bank_management_system/src/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+#include <ctype.h>
 #include "../include/account.h"
 
 
@@ -9,12 +10,12 @@ typedef struct {
 } Bank;
 
 int validate_acc_num(char acc_num[10]);
-int validate_pin(int pin);
+int validate_pin(char pin[5]);
 
 
 int main() {
-	int option, pin;
-	char acc_num[10];
+	int option;
+	char acc_num[10], pin[5];
 
 	printf("=== Core Banking System ===\n");
 
@@ -27,20 +28,39 @@ int main() {
 	printf("Enter Account Number: ");
 	scanf("%s", &acc_num);
 
-	if (validate_acc_num(acc_num) == 1) {
+	if (validate_acc_num(acc_num) != 0) {
 		printf("Error: Invalid Account Number\n");
 		return 1;
 	}
 
 	printf("Enter Pin: ");
-	scanf("%d", &pin);
+	scanf("%s", &pin);
 
-	if (validate_pin(pin) == 1) {
+	if (validate_pin(pin) != 0 ) {
 		printf("Error: Invalid Pin\n");
 		return 1;
 	}
 }
 
 
-int validate_acc_num(char acc_num[10]);
-int validate_pin(int pin);
+int validate_acc_num(char acc_num[10]) {
+	int length = strlen(acc_num);
+
+	if (length != 9) {
+		return 1;
+	}
+
+	for (int i = 0; i < length; i++) {
+		if (isdigit(acc_num[i]) == 0) {
+			return 1;
+		}
+	}
+
+	return 0;
+}
+
+int validate_pin(char pin[5]) {
+	printf("%d", strlen(pin));
+
+	return 0;
+}
