5da6a9539b06d9022e0b18c4df1fa009d537e057
Sam Kaveh <seriousemail178@gmail.com>
2025-02-22 12:00:38 +1100

    Working on main + refactor

--- bank_management_system/src/main.c
+++ bank_management_system/src/main.c
@@ -8,63 +8,39 @@ typedef struct {
 	int acc_num;
 } Bank;
 
-Bank* createBank();
+int validate_acc_num(char acc_num[10]);
+int validate_pin(int pin);
 
 
 int main() {
-	char test_num[10];
-	float test_balance;
-	float buff;
-	Account* acc;
-	Bank* test_bank;
+	int option, pin;
+	char acc_num[10];
 
-	printf("Enter Account number: ");
-	scanf("%s", &test_num);
+	printf("=== Core Banking System ===\n");
 
-	// Account Number Input Validation
-	if (strlen(test_num) != 9) {
-		printf("Error: Account number must be 9 Digits\n");
+	printf("1) Create Account\n");
+	printf("2) Access Account\n");
+
+	printf("Select an Option: ");
+	scanf("%d", &option);
+
+	printf("Enter Account Number: ");
+	scanf("%s", &acc_num);
+
+	if (validate_acc_num(acc_num) == 1) {
+		printf("Error: Invalid Account Number\n");
 		return 1;
 	}
 
-	printf("Enter Account Balance: ");
-	scanf("%f", &test_balance);
+	printf("Enter Pin: ");
+	scanf("%d", &pin);
 
-	acc = createAccount(test_num, test_balance);
-
-	// Account Creation Error Check
-	if (acc == NULL) {
-		printf("Error: Account creation failed\n");
+	if (validate_pin(pin) == 1) {
+		printf("Error: Invalid Pin\n");
 		return 1;
 	}
-
-	test_bank = createBank();
-	
-	depositAccount(acc, 1500);
-
-	if (acc->balance != 3000) {
-		printf("Error: Deposit Failed %.2f", acc->balance);
-		return 1;
-	}
-
-	buff = withdrawAccount(acc, 1500);
-
-	// Account Deposit Fail Check
-	if (buff == -1) {
-		printf("Error: Not enough Money\n");
-		return -1;
-	}
-
-	printf("=== Test Account ===\n");
-	printf("Account Number: %s\n", acc->num);
-	printf("Account Balance: %.2f\n", acc->balance);
-
-	return 0;
 }
 
 
-Bank* createBank() {
-	static Bank new_bank;
-	new_bank.acc_num = 0;
-	return &new_bank;
-}
+int validate_acc_num(char acc_num[10]);
+int validate_pin(int pin);
