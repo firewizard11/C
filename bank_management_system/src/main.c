9e729f562b045f993aa8022c4899d4ca4971979e
Sam Kaveh <seriousemail178@gmail.com>
2025-02-22 12:59:43 +1100

    Added check for invalid option and test for account creation option

--- bank_management_system/src/main.c
+++ bank_management_system/src/main.c
@@ -2,19 +2,7 @@
 #include <string.h>
 #include <ctype.h>
 #include "../include/account.h"
-
-
-typedef struct {
-	Account acc_list[10];
-	int num_acc;
-} Bank;
-
-Bank createBank(int num_acc);
-int searchBank(Bank* bank, char acc_num[10]);
-void addAccount(Bank* bank, Account acc);
-
-int validate_acc_num(char acc_num[10]);
-int validate_pin(char pin[5]);
+#include "../include/bank.h"
 
 
 int main() {
@@ -29,6 +17,11 @@ int main() {
 
 	printf("Select an Option: ");
 	scanf("%d", &option);
+
+	if (!(option == 1) && !(option == 2)) {
+		printf("Error: Invalid Option\n");
+		return 1;
+	}
 
 	printf("Enter Account Number: ");
 	scanf("%s", &acc_num);
@@ -46,8 +39,6 @@ int main() {
 		return 1;
 	}
 
-	addAccount(&bank, *createAccount("123456789", "0000", 0));
-
 	switch (option) {
 		case 1:
 			if (searchBank(&bank, acc_num) != -1) {
@@ -58,64 +49,20 @@ int main() {
 			addAccount(&bank, *createAccount(acc_num, pin, 0));
 			printf("Account Created\n");
 			break;
-	}
 
-	return 0;
-}
+		case 2:
+			idx_acc = searchBank(&bank, acc_num);
 
-
-Bank createBank(int num_acc) {
-	Bank new_bank;
-	new_bank.num_acc = num_acc;
-	return new_bank;
-}
-
-int searchBank(Bank* bank, char acc_num[10]) {
-	if (bank->num_acc == 0) {
-		return -1;
-	}
-
-	for (int i = 0; i < bank->num_acc; i++) {
-		if (strcmp(bank->acc_list[i].num, acc_num) == 0) {
-			return i;
-		}
-	}
-
-	return -1;
-}
-
-void addAccount(Bank* bank, Account acc) {
-	bank->acc_list[bank->num_acc] = acc;
-	bank->num_acc++;
-}
-
-int validate_acc_num(char acc_num[10]) {
-	int length = strlen(acc_num);
-
-	if (length != 9) {
-		return 1;
-	}
-
-	for (int i = 0; i < length; i++) {
-		if (isdigit(acc_num[i]) == 0) {
-			return 1;
-		}
-	}
-
-	return 0;
-}
-
-int validate_pin(char pin[5]) {
-	int length = strlen(pin);
-
-	if (length != 4) {
-		return 1;
-	}
-
-	for (int i = 0; i < length; i++) {
-		if (isdigit(pin[i]) == 0) {
-			return 1;
-		}
+			switch (idx_acc) {
+				case -1:
+					printf("Account Not Found\n");
+					break;
+				case 0:
+					printf("Account Number: %s\n", bank.acc_list[idx_acc].num);
+					printf("Pin: %s\n", bank.acc_list[idx_acc].pin);
+					printf("Balance: %.2f\n", bank.acc_list[idx_acc].balance);
+					break;
+			}
 	}
 
 	return 0;
