e3b901d292bc02408767e0c6e8d23d6ae5e5fc1f
Sam Kaveh <seriousemail178@gmail.com>
2025-02-23 15:41:06 +1100

    Factored Out Account operations to it's own function to reduce main() size

--- bank_management_system/src/main.c
+++ bank_management_system/src/main.c
@@ -5,20 +5,18 @@
 #include "../include/bank.h"
 
 
+int acc_ops(Bank* bank, char acc_num[10], char pin[5]);
+
+
 int main() {
-	int option, option2, option3, idx_acc;
-	float amount;
+	int option, op_result;
 	char acc_num[10], pin[5];
 	Bank bank = createBank(0);
-	Account* curr_acc;
-
 	loadBank(&bank);
 
 	printf("=== Core Banking System ===\n");
-
 	printf("1) Create Account\n");
 	printf("2) Access Account\n");
-
 	printf("Select an Option: ");
 	scanf("%d", &option);
 
@@ -29,7 +27,6 @@ int main() {
 
 	printf("Enter Account Number: ");
 	scanf("%s", &acc_num);
-
 	if (validate_acc_num(acc_num) != 0) {
 		printf("Error: Invalid Account Number\n");
 		return 1;
@@ -37,14 +34,12 @@ int main() {
 
 	printf("Enter Pin: ");
 	scanf("%s", &pin);
-
 	if (validate_pin(pin) != 0 ) {
 		printf("Error: Invalid Pin\n");
 		return 1;
 	}
 
 	switch (option) {
-
 		case 1:
 			if (searchBank(&bank, acc_num) != -1) {
 				printf("Error: Account Already Exists\n");
@@ -56,58 +51,10 @@ int main() {
 			break;
 
 		case 2:
-			idx_acc = searchBank(&bank, acc_num);
+			op_result = acc_ops(&bank, acc_num, pin);
 
-			if (idx_acc == -1) {
-				printf("Account Not Found\n");
-				return 1;
-			}
-
-			if (strcmp(bank.acc_list[idx_acc].pin, pin) != 0) {
-				printf("Error: PIN doesn't Match\n");
-				return 1;
-			} 
-			
-			curr_acc = &bank.acc_list[idx_acc];
-
-			while (1) {
-				printf("1) Deposit\n");
-				printf("2) Withdraw\n");
-				printf("3) Check Balance\n");
-				printf("Select an Option: ");
-				scanf("%d", &option2);
-	
-				if (option2 != 1 && option2 != 2 && option2 != 3) {
-					printf("Error: Invalid Option\n");
-					continue;
-				}
-	
-				switch (option2) {
-					case 1:
-						printf("Enter Amount: ");
-						scanf("%f", &amount);
-						depositAccount(curr_acc, amount);
-						break;
-					case 2:
-						printf("Enter Amount: ");
-						scanf("%f", &amount);
-						withdrawAccount(curr_acc, amount);
-						break;
-					case 3:
-						printf("Balance: %.2f\n", curr_acc->balance);
-						break;
-				}
-
-				printf("1) More Operations\n");
-				printf("2) Quit\n");
-				printf("Select an Option: ");
-				scanf("%d", &option3);
-
-				if (option3 == 1) {
-					continue;
-				} else if (option3 == 2) {
-					break;
-				}
+			if (op_result == 1) {
+				printf("Error: Account Operation Failure\n");
 			}
 			
 			break;
@@ -115,5 +62,70 @@ int main() {
 
 
 	saveBank(&bank);
+	return 0;
+}
+
+
+int acc_ops(Bank* bank, char acc_num[10], char pin[5]) {
+	int idx_acc, option, option2;
+	float amount;
+	Account* curr_acc;
+	
+	idx_acc = searchBank(bank, acc_num);
+	if (idx_acc == -1) {
+		printf("Account Not Found\n");
+		return 1;
+	}
+
+	if (strcmp(bank->acc_list[idx_acc].pin, pin) != 0) {
+		printf("Error: PIN doesn't Match\n");
+		return 1;
+	} 
+	
+	curr_acc = &bank->acc_list[idx_acc];
+
+	while (1) {
+		printf("1) Deposit\n");
+		printf("2) Withdraw\n");
+		printf("3) Check Balance\n");
+		printf("Select an Option: ");
+		scanf("%d", &option);
+
+		if (option != 1 && option != 2 && option != 3) {
+			printf("Error: Invalid Option\n");
+			continue;
+		}
+
+		switch (option) {
+			case 1:
+				printf("Enter Amount: ");
+				scanf("%f", &amount);
+				depositAccount(curr_acc, amount);
+				break;
+			case 2:
+				printf("Enter Amount: ");
+				scanf("%f", &amount);
+				withdrawAccount(curr_acc, amount);
+				break;
+			case 3:
+				printf("Balance: %.2f\n", curr_acc->balance);
+				break;
+		}
+
+		printf("1) More Operations\n");
+		printf("2) Quit\n");
+		printf("Select an Option: ");
+		scanf("%d", &option2);
+
+		if (option2 == 1) {
+			continue;
+		} else if (option2 == 2) {
+			break;
+		} else {
+			printf("Error: Invalid Option\n");
+			break;
+		}
+	}
+
 	return 0;
 }
